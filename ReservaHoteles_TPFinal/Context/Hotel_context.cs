4161ba3b7d0896d87d6c22b3200220343972bdb8
Denise-E <denueichen@gmail.com>
2023-06-05 10:23:06 -0300

    Adding onModelCreating data

--- ReservaHoteles_TPFinal/Context/Hotel_context.cs
+++ ReservaHoteles_TPFinal/Context/Hotel_context.cs
@@ -1,10 +1,86 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ReservaHoteles_TPFinal.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ReservaHoteles_TPFinal.Context
 {
     public class Hotel_context: DbContext
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Habitacion>().HasData(
+                new Habitacion()
+                {
+                    Id = 1,
+                    numHabitacion = 11,
+                    capacidad = 2,
+                    costoPorDia = 6500,
+                    ocupada = true
+                },
+
+                new Habitacion()
+                {
+                    Id = 2,
+                    numHabitacion = 12,
+                    capacidad = 5,
+                    costoPorDia = 8000,
+                    ocupada = false
+                },
+
+                new Habitacion()
+                {
+                    Id = 3,
+                    numHabitacion = 13,
+                    capacidad = 3,
+                    costoPorDia = 6500,
+                    ocupada = false
+                },
+
+                new Habitacion()
+                {
+                    Id = 4,
+                    numHabitacion = 14,
+                    capacidad = 2,
+                    costoPorDia = 6000,
+                    ocupada = false
+                }
+             );
+
+            builder.Entity<MedioDePago>().HasData(
+                new MedioDePago()
+                {
+                    Id = 1,
+                    nombre = "Credito",
+                },
+
+                new MedioDePago()
+                {
+                    Id=2,
+                    nombre = "Debito"
+                },
+
+                new MedioDePago()
+                {
+                    Id = 3,
+                    nombre = "Mercado Pagp"
+                },
+
+                new MedioDePago()
+                {
+                    Id = 4,
+                    nombre = "Trsnaferencia"
+                },
+
+                new MedioDePago()
+                {
+                    Id = 5,
+                    nombre = "Efectivo"
+                }
+            );
+        }
+
         public DbSet<Habitacion> Habitaciones { get; set; }
         public DbSet<Huesped> Huespedes { get; set; }
         public DbSet<MedioDePago> MediosDePago { get; set; }
---
+++ ReservaHoteles_TPFinal/Migrations/20230605131329_onModelCreatingData.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ReservaHoteles_TPFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class onModelCreatingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Habitaciones",
+                columns: new[] { "Id", "capacidad", "costoPorDia", "numHabitacion", "ocupada" },
+                values: new object[,]
+                {
+                    { 1, 2, 6500.0, 11, true },
+                    { 2, 5, 8000.0, 12, false },
+                    { 3, 3, 6500.0, 13, false },
+                    { 4, 2, 6000.0, 14, false }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MediosDePago",
+                columns: new[] { "Id", "nombre" },
+                values: new object[,]
+                {
+                    { 1, "Credito" },
+                    { 2, "Debito" },
+                    { 3, "Mercado Pagp" },
+                    { 4, "Trsnaferencia" },
+                    { 5, "Efectivo" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Habitaciones",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Habitaciones",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Habitaciones",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Habitaciones",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "MediosDePago",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MediosDePago",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MediosDePago",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MediosDePago",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "MediosDePago",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
